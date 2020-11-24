c3ac7cb1a51c569da59cbe196427d9ba92903a6b
Jay Idrees <idreesjay@gmail.com>
2020-11-24 00:56:58 -0500

    added the updateOne function in orm.js

--- config/orm.js
+++ config/orm.js
@@ -5,7 +5,7 @@ const connection=require("./connection");
 
 const orm={
 //Select all object to query  all columns from a specified table
-// Using table name in query to invoke its contents and a callback function in the body of the function selectALL
+// Using table name in query to invoke its contents and a callback function in the body of the function selectALL to capture the result object
     selectAll: function(table_name, cb) {
         var queryString = "SELECT * FROM " + table_name + ";";
         connection.query(queryString, function(err, result) {
@@ -21,9 +21,11 @@ const orm={
         var queryString = "INSERT INTO " + table;
     
         queryString += " (";
+        // This code wwill paste the name of the columns 
         queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (";
+        //
         queryString += printQuestionMarks(vals.length);
         queryString += ") ";
     
@@ -37,8 +39,24 @@ const orm={
             cb(result);
         });
     },
-
-
+// Adding the updateOne function
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+        
+            cb(result);
+        });
+    }  
 
 
 
