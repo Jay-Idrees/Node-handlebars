9643c3fb4eef7e88fcb075f5232aed3806133dda
Jay Idrees <idreesjay@gmail.com>
2020-11-24 01:57:41 -0500

    Completed burger.js model code with selectAll, InsertOne and UpdateOne functions

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,31 @@
-const orm=require("../config/orm")
+
+const orm = require("../config/orm.js");
+
+
+const Burger = {
+    selectAll: function(cb) {
+        // Assigning arguments to the select all function in the ORM, passing the table name 'burger' from the burger_db. Note the use of OOP. I have created a burger object and then stored objects named according to their call back functions
+        // THis is basically readymade code for excuting general functions  with specific arguemtns to run MYSQL query
+        // For instance SelectAll: function(table_name, cb), here table_name is 'burgers' and function(result) is call back
+      orm.selectAll("burgers", function(result) {
+        cb(result);
+      });
+    },
+    // Similar as above
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(result) {
+          // Note that the result here will contain an object burgers with column name (e-g burger_name, id, devoured) and their values (name of the burger, id value and true/false for devoured)
+        cb(result);
+      });
+    },
+    // Similar as above
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(result) {
+        cb(result);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller
+  module.exports = Burger;
+  
