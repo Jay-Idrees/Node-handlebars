2826c11dc3b65a87389ff766604162d78681948d
Jay Idrees <idreesjay@gmail.com>
2020-11-24 02:51:06 -0500

    Specified the get request in burger_controller to execute query for selectALL from the burger Model, packing the query result into object for display by handle bars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,5 +2,19 @@
 // Setting up dependencies
 const express = require("express");
 const router = express.Router();
-const Burger=require('../models/burger');
+
+// Import the model (burger.js) to use its database functions.
+const Burger = require("../models/burger.js");
+
+// Routes- 
+//This will get all the burgers using the selectall burger controller, package the burger into 'hbs object' and then send it to the handle bars
+router.get("/", function(req, res) {
+  Burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
 
